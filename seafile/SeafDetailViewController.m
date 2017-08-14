6c9d692ee7db428cb8a2f4d4d7ee23a78f70b48e
lilthree <87437904@qq.com>
2017-08-14 10:02:09 +0800

    fix image rotate problem

--- seafile/SeafDetailViewController.m
+++ seafile/SeafDetailViewController.m
@@ -349,10 +349,22 @@ enum SHARE_STATUS {
 {
     [super viewWillLayoutSubviews];
     CGRect r = CGRectMake(self.view.frame.origin.x, 64, self.view.frame.size.width, self.view.frame.size.height - 64);
+    if (IsIpad()) {
+        r = CGRectMake(self.view.frame.origin.x, 0, self.view.frame.size.width, self.view.frame.size.height - 0);
+    }
     if (self.state == PREVIEW_QL_SUBVIEW) {
         self.qlSubViewController.view.frame = r;
     } else if (self.state == PREVIEW_PHOTO){
-        self.mwPhotoBrowser.view.frame = r;
+        if (IsIpad()) {
+            self.mwPhotoBrowser.view.frame = r;
+        } else {
+            UIDeviceOrientation orientation = (UIDeviceOrientation)[UIApplication sharedApplication].statusBarOrientation;
+            if (orientation == UIDeviceOrientationPortrait || orientation == UIDeviceOrientationPortraitUpsideDown) {
+                self.mwPhotoBrowser.view.frame = r;
+            } else {
+                self.mwPhotoBrowser.view.frame = CGRectMake(self.view.frame.origin.x, 32, self.view.frame.size.width, self.view.frame.size.height - 32);
+            }
+        }
     } else {
         if (self.view.subviews.count > 1) {
             UIView *v = [self.view.subviews objectAtIndex:0];
