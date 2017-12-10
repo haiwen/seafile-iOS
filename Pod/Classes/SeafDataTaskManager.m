431e1473e39a873430642807faf6140243a7e4af
lilthree <87437904@qq.com>
2017-12-10 18:40:47 +0800

    save download&upload task and continue next time launching

--- Pod/Classes/SeafDataTaskManager.m
+++ Pod/Classes/SeafDataTaskManager.m
@@ -10,6 +10,10 @@
 #import "SeafDir.h"
 #import "Debug.h"
 #import "SeafFile.h"
+#import "SeafStorage.h"
+
+#define ALL_UPLOAD @"allUploadTask"
+#define ALL_DOWNLOAD @"allDownloadTask"
 
 @interface SeafDataTaskManager()
 
@@ -65,6 +69,7 @@
 {
     SeafAccountTaskQueue *accountQueue = [self getAccountQueueWithIndentifier:file.accountIdentifier];
     [accountQueue addUploadTask:file];
+    [self saveToTaskStorage:file withIndentifier:file.accountIdentifier];
     if (self.trySyncBlock) {
         self.trySyncBlock(file);
     }
@@ -163,6 +168,7 @@
 {
     SeafAccountTaskQueue *accountQueue = [self getAccountQueueWithIndentifier:dfile.accountIdentifier];
     [accountQueue addFileDownloadTask:dfile];
+    [self saveToTaskStorage:dfile withIndentifier:dfile.accountIdentifier];
     if (self.trySyncBlock) {
         self.trySyncBlock(dfile);
     }
@@ -189,9 +195,11 @@
             __weak typeof(self) weakSelf = self;
             accountQueue.uploadQueue.taskCompleteBlock = ^(id<SeafTask>  _Nonnull task, BOOL result) {
                 if (weakSelf.finishBlock)  weakSelf.finishBlock(task);
+                [weakSelf removeTaskInStorage:task withIndentifier:task.accountIdentifier];
             };
             accountQueue.fileQueue.taskCompleteBlock = ^(id<SeafTask>  _Nonnull task, BOOL result) {
                 if (weakSelf.finishBlock)  weakSelf.finishBlock(task);
+                [weakSelf removeTaskInStorage:task withIndentifier:task.accountIdentifier];
             };
 
             [self.accountQueueDict setObject:accountQueue forKey:identifier];
@@ -212,11 +220,89 @@
     }
 }
 
+- (void)saveToTaskStorage:(id)task withIndentifier:(NSString*)accountIdentifier {
+    NSString *key = [NSString new];
+    NSDictionary *dict = [self convertTaskToDict:task];
+    if ([task isKindOfClass:[SeafFile class]]) {
+        key = [NSString stringWithFormat:@"%@/%@",ALL_DOWNLOAD,accountIdentifier];
+    } else if ([task isKindOfClass:[SeafUploadFile class]]) {
+        key = [NSString stringWithFormat:@"%@/%@",ALL_UPLOAD,accountIdentifier];
+    }
+    
+    NSMutableArray *downloadStorage = [NSMutableArray arrayWithArray:[SeafStorage.sharedObject objectForKey:key]];
+    if (![downloadStorage containsObject:dict]) {
+        [downloadStorage addObject:dict];
+        [SeafStorage.sharedObject setObject:downloadStorage forKey:key];
+    }
+}
+
+- (void)removeTaskInStorage:(id)task withIndentifier:(NSString*)accountIdentifier {
+    NSString *key = [NSString new];
+    NSDictionary *dict = [self convertTaskToDict:task];
+    if ([task isKindOfClass:[SeafFile class]]) {
+        key = [NSString stringWithFormat:@"%@/%@",ALL_DOWNLOAD,accountIdentifier];
+    } else if ([task isKindOfClass:[SeafUploadFile class]]) {
+        key = [NSString stringWithFormat:@"%@/%@",ALL_UPLOAD,accountIdentifier];
+    }
+    
+    NSMutableArray *downloadStorage = [NSMutableArray arrayWithArray:[SeafStorage.sharedObject objectForKey:key]];
+    if ([downloadStorage containsObject:dict]) {
+        [downloadStorage removeObject:dict];
+        [SeafStorage.sharedObject setObject:downloadStorage forKey:key];
+    }
+}
+
+- (NSDictionary*)convertTaskToDict:(id)task {
+    NSDictionary *dict = [NSDictionary dictionary];
+    if ([task isKindOfClass:[SeafFile class]]) {
+        SeafFile *file = (SeafFile*)task;
+        dict = @{@"oid":file.oid,@"repoId":file.repoId,@"name":file.name,@"path":file.path,@"mtime":[NSNumber numberWithLong:file.mtime],@"size":[NSNumber numberWithLong:file.filesize]};
+    } else if ([task isKindOfClass:[SeafUploadFile class]]) {
+        SeafUploadFile *ufile = (SeafUploadFile*)task;
+        dict = @{@"lpath":ufile.lpath,@"oid":ufile.udir.oid,@"repoId":ufile.udir.repoId,@"name":ufile.udir.name,@"path":ufile.udir.path,@"perm":ufile.udir.perm,@"mime":ufile.udir.mime};
+    }
+    return dict;
+}
+
+- (void)startLastTimeUnfinshTaskWithConnection:(SeafConnection *)conn {
+    NSString *downloadKey = [NSString stringWithFormat:@"%@/%@",ALL_DOWNLOAD,conn.accountIdentifier];
+    NSArray *downloadTasks = [SeafStorage.sharedObject objectForKey:downloadKey];
+    if (downloadTasks.count > 0) {
+        for (NSDictionary *dict in downloadTasks) {
+            SeafFile *file = [[SeafFile alloc] initWithConnection:conn oid:[dict objectForKey:@"oid"] repoId:[dict objectForKey:@"repoId"] name:[dict objectForKey:@"name"] path:[dict objectForKey:@"path"] mtime:[[dict objectForKey:@"mtime"] longLongValue] size:[[dict objectForKey:@"size"] longLongValue]];
+            [self addFileDownloadTask:file];
+        }
+    }
+    
+    NSString *uploadKey = [NSString stringWithFormat:@"%@/%@",ALL_UPLOAD,conn.accountIdentifier];
+    NSArray *uploadTasks = [SeafStorage.sharedObject objectForKey:uploadKey];
+    if (uploadTasks.count > 0) {
+        for (NSDictionary *dict in uploadTasks) {
+            SeafUploadFile *ufile = [[SeafUploadFile alloc] initWithPath:[dict objectForKey:@"lpath"]];
+            SeafDir *udir = [[SeafDir alloc] initWithConnection:conn oid:[dict objectForKey:@"oid"] repoId:[dict objectForKey:@"repoId"] perm:[dict objectForKey:@"perm"] name:[dict objectForKey:@"name"] path:[dict objectForKey:@"path"] mime:[dict objectForKey:@"mime"]];
+            ufile.udir = udir;
+            [self addUploadTask:ufile];
+        }
+    }
+}
+
+- (void)removeDownloadTaskInStoragewithIndentifier:(NSString *)accountIdentifier {
+    NSString *key = [NSString stringWithFormat:@"%@/%@",ALL_DOWNLOAD,accountIdentifier];
+    [SeafStorage.sharedObject removeObjectForKey:key];
+}
+
+- (void)removeUploadTaskInStoragewithIndentifier:(NSString*)accountIdentifier {
+    NSString *key = [NSString stringWithFormat:@"%@/%@",ALL_UPLOAD,accountIdentifier];
+    [SeafStorage.sharedObject removeObjectForKey:key];
+}
+
 - (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 @end
+
+#pragma mark- SeafAccountTaskQueue
 
 @implementation SeafAccountTaskQueue
 
