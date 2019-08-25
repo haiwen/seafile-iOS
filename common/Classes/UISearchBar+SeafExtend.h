bf235c0723eefb6cded8e16acab72fbcf1177b2a
lilthree <lilthreee@icloud.com>
2019-08-25 22:55:43 +0800

    Fix searchbar's frame

---
+++ common/Classes/UISearchBar+SeafExtend.h
@@ -0,0 +1,17 @@
+//
+//  UISearchBar+SeafExtend.h
+//  seafileApp
+//
+//  Created by three on 2019/8/25.
+//  Copyright © 2019 Seafile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UISearchBar (SeafExtend)
+
+@end
+
+NS_ASSUME_NONNULL_END
