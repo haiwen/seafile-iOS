f2e5c45bd2c358a22d7d40b563194143f84bdfcf
three <lilthreee@icloud.com>
2020-08-16 22:24:50 +0800

    Fix import files error

--- common/Classes/SeafInputItemsProvider.m
+++ common/Classes/SeafInputItemsProvider.m
@@ -18,6 +18,7 @@
 @property (nonatomic, strong) dispatch_group_t group;
 @property (nonatomic, strong) NSMutableArray *ufiles;
 @property (nonatomic, strong) CompleteBlock completeBlock;
+@property (nonatomic, copy) NSString *tmpdir;
 
 @end
 
@@ -46,86 +47,97 @@
         provider.completeBlock(false, nil);
         return;
     }
+    provider.tmpdir = tmpdir;
     
     for (NSExtensionItem *item in extensionContext.inputItems) {
         for (NSItemProvider *itemProvider in item.attachments) {
-            dispatch_group_enter(provider.group);
-            dispatch_barrier_async(queue, ^{
-                Debug("itemProvider: %@", itemProvider);
+            Debug("itemProvider: %@", itemProvider);
+            dispatch_group_async(provider.group, queue, ^{
                 if ([itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeItem]) {
+                    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
                     [itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeItem options:nil completionHandler:^(id<NSSecureCoding, NSObject>  _Nullable item, NSError * _Null_unspecified error) {
                         if (!error) {
-                            if ([item isKindOfClass:[UIImage class]]) {
-                                UIImage *image = (UIImage *)item;
-                                NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-                                [formatter setDateFormat:@"yyyy'-'MM'-'dd'T'HH'-'mm'-'ss"];
-                                
-                                NSString *name = [NSString stringWithFormat:@"IMG_%@.JPG", [formatter stringFromDate:[NSDate date]] ];
-                                NSData *data = [provider UIImageToDataJPEG:image];
-                                NSURL *targetUrl = [NSURL fileURLWithPath:[tmpdir stringByAppendingPathComponent:name]];
-                                
-                                [provider loadPreviewImageWith:itemProvider writeData:data toTargetUrl:targetUrl];
-                            } else if ([item isKindOfClass:[NSData class]]) {
-                                NSData *data = (NSData *)item;
-                                NSString *name = item.description;
-                                NSURL *targetUrl = [NSURL fileURLWithPath:[tmpdir stringByAppendingPathComponent:name]];
-                                
-                                [provider loadPreviewImageWith:itemProvider writeData:data toTargetUrl:targetUrl];
-                            } else if ([item isKindOfClass:[NSURL class]]) {
-                                NSURL *url = (NSURL *)item;
-                                NSString *name = url.lastPathComponent;
-                                NSURL *targetUrl = [NSURL fileURLWithPath:[tmpdir stringByAppendingPathComponent:name]];
-                                BOOL ret = [Utils copyFile:url to:targetUrl];
-                                if (ret) {
-                                    [provider loadPreviewImageWith:itemProvider toTargetUrl:targetUrl];
-                                } else {
-                                    [provider handleFailure];
-                                }
-                            } else if ([item isKindOfClass:[NSString class]]) {
-                                NSString *string = (NSString *)item;
-                                if (string.length > 0) {
-                                    NSString *name = [NSString stringWithFormat:@"%@.txt", item.description];
-                                    NSURL *targetUrl = [NSURL fileURLWithPath:[tmpdir stringByAppendingPathComponent:name]];
-                                    NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
-                                    [provider writeData:data toTarget:targetUrl andPrivewImage:nil];
-                                } else {
-                                    [provider handleFailure];
-                                }
-                            } else {
-                                [provider handleFailure];
-                            }
+                            [provider loadMatchingItem:item provider:itemProvider handler:^(BOOL result) {
+                                dispatch_semaphore_signal(semaphore);
+                            }];
                         } else {
-                            [provider handleFailure];
+                            [provider handleFailure:^(BOOL result) {
+                                dispatch_semaphore_signal(semaphore);
+                            }];
                         }
                     }];
+                    dispatch_semaphore_wait(semaphore, dispatch_time(DISPATCH_TIME_NOW, 5*NSEC_PER_SEC));
                 } else {
-                    [provider handleFailure];
+                    [provider handleFailure:nil];
                 }
             });
         }
-    }
+    };
     
-    dispatch_group_notify(provider.group, dispatch_get_main_queue(), ^{
+    dispatch_group_notify(provider.group, queue, ^{
         provider.completeBlock(true, provider.ufiles);
     });
 }
 
-- (void)loadPreviewImageWith:(NSItemProvider *)itemProvider writeData:(NSData *)data toTargetUrl:(NSURL *)targetUrl {
+- (void)loadMatchingItem:(id<NSSecureCoding, NSObject>)item provider:(NSItemProvider*)itemProvider handler:(ItemLoadHandler)handler {
+    Debug("load Matching items");
+    if ([item isKindOfClass:[UIImage class]]) {
+        UIImage *image = (UIImage *)item;
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        [formatter setDateFormat:@"yyyy'-'MM'-'dd'T'HH'-'mm'-'ss"];
+        
+        NSString *name = [NSString stringWithFormat:@"IMG_%@.JPG", [formatter stringFromDate:[NSDate date]] ];
+        NSData *data = [self UIImageToDataJPEG:image];
+        NSURL *targetUrl = [NSURL fileURLWithPath:[self.tmpdir stringByAppendingPathComponent:name]];
+        
+        [self loadPreviewImageWith:itemProvider writeData:data toTargetUrl:targetUrl handler:handler];
+    } else if ([item isKindOfClass:[NSData class]]) {
+        NSData *data = (NSData *)item;
+        NSString *name = item.description;
+        NSURL *targetUrl = [NSURL fileURLWithPath:[self.tmpdir stringByAppendingPathComponent:name]];
+        
+        [self loadPreviewImageWith:itemProvider writeData:data toTargetUrl:targetUrl handler:handler];
+    } else if ([item isKindOfClass:[NSURL class]]) {
+        NSURL *url = (NSURL *)item;
+        NSString *name = url.lastPathComponent;
+        NSURL *targetUrl = [NSURL fileURLWithPath:[self.tmpdir stringByAppendingPathComponent:name]];
+        BOOL ret = [Utils copyFile:url to:targetUrl];
+        if (ret) {
+            [self loadPreviewImageWith:itemProvider toTargetUrl:targetUrl handler:handler];
+        } else {
+            [self handleFailure:handler];
+        }
+    } else if ([item isKindOfClass:[NSString class]]) {
+        NSString *string = (NSString *)item;
+        if (string.length > 0) {
+            NSString *name = [NSString stringWithFormat:@"%@.txt", item.description];
+            NSURL *targetUrl = [NSURL fileURLWithPath:[self.tmpdir stringByAppendingPathComponent:name]];
+            NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
+            [self writeData:data toTarget:targetUrl andPrivewImage:nil handler:handler];
+        } else {
+            [self handleFailure:handler];
+        }
+    } else {
+        [self handleFailure:handler];
+    }
+}
+
+- (void)loadPreviewImageWith:(NSItemProvider *)itemProvider writeData:(NSData *)data toTargetUrl:(NSURL *)targetUrl handler:(ItemLoadHandler)handler {
     [itemProvider loadPreviewImageWithOptions:nil completionHandler:^(id<NSSecureCoding, NSObject>  _Nullable item, NSError * _Null_unspecified error) {
         if (!error && item && [item isKindOfClass:[UIImage class]]) {
-            [self writeData:data toTarget:targetUrl andPrivewImage:(UIImage*)item];
+            [self writeData:data toTarget:targetUrl andPrivewImage:(UIImage*)item handler:handler];
         } else {
-            [self writeData:data toTarget:targetUrl andPrivewImage:nil];
+            [self writeData:data toTarget:targetUrl andPrivewImage:nil handler:handler];
         }
     }];
 }
 
-- (void)loadPreviewImageWith:(NSItemProvider *)itemProvider toTargetUrl:(NSURL *)targetUrl {
+- (void)loadPreviewImageWith:(NSItemProvider *)itemProvider toTargetUrl:(NSURL *)targetUrl handler:(ItemLoadHandler)handler {
     [itemProvider loadPreviewImageWithOptions:nil completionHandler:^(id<NSSecureCoding, NSObject>  _Nullable item, NSError * _Null_unspecified error) {
         if (!error && item && [item isKindOfClass:[UIImage class]]) {
-            [self handleFile:targetUrl andPriview:(UIImage*)item];
+            [self handleFile:targetUrl andPriview:(UIImage*)item handler:handler];
         } else {
-            [self handleFile:targetUrl andPriview:nil];
+            [self handleFile:targetUrl andPriview:nil handler:handler];
         }
     }];
 }
@@ -137,20 +149,20 @@
     }
 }
 
-- (void)writeData:(NSData *)data toTarget:(NSURL *)targetUrl andPrivewImage:(UIImage *)preview{
+- (void)writeData:(NSData *)data toTarget:(NSURL *)targetUrl andPrivewImage:(UIImage *)preview handler:(ItemLoadHandler)handler {
     BOOL ret = [data writeToURL:targetUrl atomically:true];
     if (ret) {
-        [self handleFile:targetUrl andPriview:preview];
+        [self handleFile:targetUrl andPriview:preview handler:handler];
     } else {
-        [self handleFailure];
+        [self handleFailure:handler];
     }
 }
 
-- (void)handleFile:(NSURL *)url andPriview:(UIImage *)preview{
+- (void)handleFile:(NSURL *)url andPriview:(UIImage *)preview handler:(ItemLoadHandler)handler {
     Debug("Received file : %@", url);
     if (!url) {
         Warning("Failed to load file.");
-        [self handleFailure];
+        [self handleFailure:handler];
         return;
     }
     Debug("Upload file %@ %lld", url, [Utils fileSizeAtPath1:url.path]);
@@ -158,13 +170,16 @@
     if (preview) {
         ufile.previewImage = preview;
     }
-    dispatch_barrier_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        [self.ufiles addObject:ufile];
-    });
-    dispatch_group_leave(self.group);
+    [self.ufiles addObject:ufile];
+    if (handler) {
+        handler(true);
+    }
 }
 
-- (void)handleFailure {
+- (void)handleFailure:(ItemLoadHandler)handler {
+    if (handler) {
+        handler(false);
+    }
     self.completeBlock(false, nil);
 }
 
