8a5586f6c0ce04042450c8df63bb89a4dd867817
henry9610 <henry9610@hotmail.com>
2025-07-03 17:49:25 +0800

    Adopt streaming video playback

--- seafile/SeafFileViewController.m
+++ seafile/SeafFileViewController.m
@@ -46,6 +46,9 @@
 #import "SeafPhotoGalleryViewController.h"
 #import <MobileCoreServices/MobileCoreServices.h>
 #import <UniformTypeIdentifiers/UniformTypeIdentifiers.h>
+#import <AVKit/AVKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import "Version.h"
 
 #define kCustomTabToolWithTopPadding 15
 #define kCustomTabToolButtonHeight 40
@@ -476,8 +479,44 @@ enum {
 
         id<SeafPreView> item = (id<SeafPreView>)_curEntry;
 
+       if ([self isCurrentFileVideo:item]) {
+           SeafFile *file = (SeafFile *)item;
+           // Use custom SeafActionSheet style
+           NSArray *titles = @[NSLocalizedString(@"Play", @"Seafile"), NSLocalizedString(@"Download", @"Seafile")];
+           SeafActionSheet *sheet = [SeafActionSheet actionSheetWithoutCancelWithTitles:titles];
+           sheet.targetVC = self;
+
+           __weak typeof(self) weakSelf = self;
+           [sheet setButtonPressedBlock:^(SeafActionSheet * _Nonnull actionSheet, NSIndexPath * _Nonnull idx) {
+               __strong typeof(weakSelf) self = weakSelf;
+               [actionSheet dismissAnimated:YES];
+               if (idx.row == 0) { // Play
+                   [self playVideoFile:file];
+               } else if (idx.row == 1) { // Download
+                   // Continue with default preview flow, same as non-video files
+                   [self.detailViewController setPreViewItem:item master:self];
+                   if (self.detailViewController.state == PREVIEW_QL_MODAL) {
+                       [self.detailViewController.qlViewController reloadData];
+                       if (IsIpad()) {
+                           [[[SeafAppDelegate topViewController] parentViewController] presentViewController:self.detailViewController.qlViewController animated:YES completion:nil];
+                       } else {
+                           [self presentViewController:self.detailViewController.qlViewController animated:YES completion:nil];
+                       }
+                   } else if (!IsIpad()) {
+                       SeafAppDelegate *appdelegate = (SeafAppDelegate *)[[UIApplication sharedApplication] delegate];
+                       [appdelegate showDetailView:self.detailViewController];
+                   }
+               }
+           }];
+
+           // anchor from the tapped cell to keep style consistent
+           UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
+           [sheet showFromView:cell];
+           return;
+       }
+
         if ([self isCurrentFileImage:item]) {
-            // 收集所有图片类型的文件
+            // Collect all image type files
             NSMutableArray *imageFiles = [NSMutableArray array];
             for (id entry in self.allItems) {
                 if ([entry conformsToProtocol:@protocol(SeafPreView)] && [(id<SeafPreView>)entry isImageFile]) {
@@ -485,19 +524,19 @@ enum {
                 }
             }
             
-            // 如果没有找到图片文件，使用旧版详情视图
+            // If no image files found, use old detail view
             if (imageFiles.count == 0) {
-                Warning("没有找到图片文件");
+                Warning("No image files found");
                 [self.detailViewController setPreViewItem:item master:self];
                 return;
             }
             
-            // 创建并设置照片库视图控制器，使用推荐的初始化方法
+            // Create and setup photo gallery view controller using recommended initialization method
             SeafPhotoGalleryViewController *gallery = [[SeafPhotoGalleryViewController alloc] initWithPhotos:imageFiles
                                                                                                 currentItem:item
                                                                                                      master:self];
             
-            // 将画廊视图控制器包装在导航控制器中，并模态显示
+            // Wrap gallery view controller in navigation controller and present modally
             UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:gallery];
             navController.modalPresentationStyle = UIModalPresentationFullScreen;
             
@@ -3396,6 +3435,107 @@ typedef NS_ENUM(NSInteger, ToolButtonTag) {
         // This will internally call `dismissCustomTabTool:` and restore insets.
         [self editDone:nil];
     }
+}
+
+- (BOOL)isCurrentFileVideo:(id<SeafPreView>)item {
+    if (![item conformsToProtocol:@protocol(SeafPreView)])
+        return NO;
+    NSString *mime = item.mime;
+    if (mime) {
+        return [mime hasPrefix:@"video/"];
+    }
+    return NO;
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context
+{
+    if ([keyPath isEqualToString:@"status"]) {
+        AVPlayerItem *playerItem = (AVPlayerItem *)object;
+        if (playerItem.status == AVPlayerItemStatusFailed) {
+            Warning(@"AVPlayerItem failed to play. Error: %@", playerItem.error);
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [SVProgressHUD dismiss];
+            });
+        } else if (playerItem.status == AVPlayerItemStatusReadyToPlay) {
+            Debug(@"AVPlayerItem is ready to play.");
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [SVProgressHUD dismiss];
+            });
+        }
+    }
+}
+
+- (void)playVideoFile:(SeafFile *)file {
+    // Always show a loading indicator first
+    [SVProgressHUD show];
+
+    // 1. Try to play the locally-cached file (exportURL keeps the original filename & extension)
+    if ([file hasCache]) {
+        NSURL *localURL = [file exportURL];
+        if (localURL && [[NSFileManager defaultManager] fileExistsAtPath:localURL.path]) {
+            Debug(@"Playing video from local cache: %@", localURL.path);
+
+            AVPlayerItem *playerItem = [AVPlayerItem playerItemWithURL:localURL];
+            // Observe status so we can dismiss the loading indicator later
+            [playerItem addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
+
+            AVPlayer *player = [AVPlayer playerWithPlayerItem:playerItem];
+            player.automaticallyWaitsToMinimizeStalling = YES;
+
+            AVPlayerViewController *playerVC = [AVPlayerViewController new];
+            playerVC.player = player;
+            [self presentViewController:playerVC animated:YES completion:^{
+                [playerVC.player play];
+            }];
+            return; // Played from cache – skip remote download
+        }
+    }
+
+    // 2. Fallback to streaming from server if no valid local cache
+    [file.connection getFileDownloadLink:file.repoId
+                                    path:file.path
+                                 success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+        [SVProgressHUD dismiss];
+        Debug(@"Get video link success. Response: %@", JSON);
+        NSString *dlink = nil;
+        if ([JSON isKindOfClass:[NSDictionary class]]) {
+            dlink = [JSON objectForKey:@"url"];
+        } else if ([JSON isKindOfClass:[NSString class]]) {
+            dlink = [(NSString *)JSON stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"\""]];
+        }
+        Debug(@"Extracted video link: %@", dlink);
+        Debug(@"Seafile token: %@", file.connection.token);
+
+        if (!dlink || dlink.length == 0) {
+            [SVProgressHUD showErrorWithStatus:NSLocalizedString(@"Failed to get video link", @"Seafile")];
+            Warning(@"Failed to get video link: dlink is nil or empty");
+            return;
+        }
+        NSURL *videoURL = [NSURL URLWithString:dlink];
+        Debug(@"Playing video from URL: %@", videoURL);
+
+        NSDictionary *headers = @{
+            @"Authorization": [NSString stringWithFormat:@"Token %@", file.connection.token],
+            @"X-Seafile-Client-Version": SEAFILE_VERSION,
+            @"X-Seafile-Platform-Version": [[UIDevice currentDevice] systemVersion]
+        };
+        AVURLAsset *asset = [AVURLAsset URLAssetWithURL:videoURL options:@{@"AVURLAssetHTTPHeaderFieldsKey": headers}];
+        AVPlayerItem *playerItem = [AVPlayerItem playerItemWithAsset:asset];
+        // Pre-buffer 3 s to reduce stutter
+        playerItem.preferredForwardBufferDuration = 3.0;
+        [playerItem addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
+        AVPlayer *player = [AVPlayer playerWithPlayerItem:playerItem];
+        player.automaticallyWaitsToMinimizeStalling = YES;
+
+        AVPlayerViewController *playerVC = [AVPlayerViewController new];
+        playerVC.player = player;
+        [self presentViewController:playerVC animated:YES completion:^{
+            [playerVC.player play];
+        }];
+    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON, NSError *error) {
+        [SVProgressHUD showErrorWithStatus:NSLocalizedString(@"Failed to get video link", @"Seafile")];
+        Warning(@"Failed to get video link. Error: %@, Response: %@", error, JSON);
+    }];
 }
 
 @end
